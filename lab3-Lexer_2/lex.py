a4a0b4aebf30e97af37f1c122059caf055eee95b
Ceres <Ceres@HWPad>
2023-04-09 15:08:59 +0800

    rename lab3

--- lab3-Lexer_2/lex.py
+++
@@ -1,8 +0,0 @@
-class Lex(object):
-    def __init__(self, tranDict, accept):
-        self.tranDict = tranDict
-        self.state = 0 # InitState
-        self.accept = accept
-        
-    def Transfer(self, inch): # inch: input char
-
